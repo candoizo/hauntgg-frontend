0e58b14563fea4f968613309ded5ca4617372ee7
candoizo <candoizo@protonmail.com>
2021-03-21 12:56:08 -0400

    :sparkles: minify hugo, ready for release?

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ require('require-dir')('./gulp')
 gulp.task('favicon', gulp.series([
   gulp.task('generate-favicon'),
     gulp.task('inject-favicon-markups'),
-    gulp.task('minify'),
+    // gulp.task('minify'),
     // gulp.task('image')
   ]),
 );
